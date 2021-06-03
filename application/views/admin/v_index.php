099b578d15ba645756175e8fc7c9127cecfdc138
Mazmiiskndr <azmiiskandar0@gmail.com>
2021-06-03 16:41:52 +0700

    Versi 1

---
+++ application/views/admin/v_index.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Produk By Mfikri.com">
+    <meta name="author" content="M Fikri Setiadi">
+
+    <title>Welcome To Point of Sale Apps</title>
+
+    <!-- Bootstrap Core CSS -->
+      <link href="<?php echo base_url().'assets/css/bootstrap.min.css'?>" rel="stylesheet">
+	    <link href="<?php echo base_url().'assets/css/style.css'?>" rel="stylesheet">
+	    <link href="<?php echo base_url().'assets/css/font-awesome.css'?>" rel="stylesheet">
+    <!-- Custom CSS -->
+      <link href="<?php echo base_url().'assets/css/4-col-portfolio.css'?>" rel="stylesheet">
+
+      <style type="text/css">
+      .bg {
+           width: 100%;
+           height: 100%;
+           position: fixed;
+           z-index: -1;
+           float: left;
+           left: 0;
+           margin-top: -20px;
+      }
+      </style>
+</head>
+
+<body>
+<img src="<?php echo base_url().'assets/img/bgmuliabaja.jpg'?>" alt="gambar" class="bg" />
+    <!-- Navigation -->
+   <?php 
+        $this->load->view('admin/menu');
+   ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class=" text-center" style="color:white;">Mulia Baja
+                
+                </h1>
+            </div>
+        </div>
+        <!-- /.row -->
+	<div class="mainbody-section text-center">
+     <?php $h=$this->session->userdata('akses'); ?>
+     <?php $u=$this->session->userdata('user'); ?>
+
+        <!-- Projects Row -->
+        <div class="row">
+         <?php if($h=='1'){ ?> 
+            <div class="col-md-3 portfolio-item">
+                <div class="menu-item blue" style="height:170px;">
+                     <a href="<?php echo base_url().'admin/penjualan'?>" data-toggle="modal">
+                           <i class="fa fa-shopping-bag"></i><p class>Penjualan Eceran</p>
+                            <!-- <p style="text-align:left;font-size:14px;padding-left:5px;">Penjualan Eceran</p> -->
+                      </a>
+                </div> 
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <div class="menu-item green" style="height:170px;">
+                     <a href="<?php echo base_url().'admin/penjualan_grosir'?>" data-toggle="modal">
+                           <i class="fa fa-cubes"></i>
+                            <p class>Penjualan Grosir</p>
+                      </a>
+                </div> 
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <div class="menu-item light-orange" style="height:170px;">
+                     <a href="<?php echo base_url().'admin/suplier'?>" data-toggle="modal">
+                           <i class="fa fa-truck"></i>
+                            <p class>Suplier</p>
+                      </a>
+                </div> 
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <div class="menu-item color" style="height:170px;">
+                     <a href="<?php echo base_url().'admin/kategori'?>" data-toggle="modal">
+                           <i class="fa fa-sitemap"></i>
+                            <p class>Kategori</p>
+                      </a>
+                </div> 
+            </div>
+            <?php }?>
+            <?php if($h=='2'){ ?> 
+            <div class="col-md-3 portfolio-item">
+                <div class="menu-item blue" style="height:170px;">
+                     <a href="#" data-toggle="modal">
+                           <i class="fa fa-shopping-cart"></i>
+                            <p style="text-align:left;font-size:14px;padding-left:5px;">Penjualan Eceran</p>
+                      </a>
+                </div> 
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <div class="menu-item green" style="height:170px;">
+                     <a href="#" data-toggle="modal">
+                           <i class="fa fa-users"></i>
+                            <p style="text-align:left;font-size:14px;padding-left:5px;">Penjualan Grosir</p>
+                      </a>
+                </div> 
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <div class="menu-item light-orange" style="height:170px;">
+                     <a href="#" data-toggle="modal">
+                           <i class="fa fa-truck"></i>
+                            <p style="text-align:left;font-size:14px;padding-left:5px;">Suplier</p>
+                      </a>
+                </div> 
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <div class="menu-item color" style="height:170px;">
+                     <a href="#" data-toggle="modal">
+                           <i class="fa fa-sitemap"></i>
+                            <p style="text-align:left;font-size:14px;padding-left:5px;">Kategori</p>
+                      </a>
+                </div> 
+            </div>
+            <?php }?>
+        </div>
+        
+        <!-- /.row -->
+
+        <!-- Projects Row -->
+        <div class="row">
+        <?php if($h=='1'){ ?> 
+            <div class="col-md-3 portfolio-item">
+                <div class="menu-item purple" style="height:170px;">
+                     <a href="<?php echo base_url().'admin/barang'?>" data-toggle="modal">
+                           <i class="fa fa-shopping-cart"></i>
+                            <p class>Barang</p>
+                      </a>
+                </div> 
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <div class="menu-item red" style="height:170px;">
+                     <a href="<?php echo base_url().'admin/pengguna'?>" data-toggle="modal">
+                           <i class="fa fa-users"></i>
+                            <p class>Pengguna</p>
+                      </a>
+                </div> 
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <div class="menu-item blue" style="height:170px;">
+                     <a href="<?php echo base_url().'admin/laporan'?>" data-toggle="modal">
+                           <i class="fa fa-bar-chart"></i>
+                            <p class>Laporan</p>
+                      </a>
+                </div> 
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <div class="menu-item light-red" style="height:170px;">
+                     <a href="<?php echo base_url().'admin/pembelian'?>" data-toggle="modal">
+                           <i class="fa fa-cubes"></i>
+                            <p class>Pembelian</p>
+                      </a>
+                </div> 
+            </div>
+            <?php }?>
+            <?php if($h=='2'){ ?> 
+            <div class="col-md-3 portfolio-item">
+                <div class="menu-item purple" style="height:170px;">
+                     <a href="<?php echo base_url().'admin/penjualan_grosir'?>" data-toggle="modal">
+                           <i class="fa fa-cubes"></i>
+                            <p style="text-align:left;font-size:14px;padding-left:5px;">Penjualan Grosir</p>
+                      </a>
+                </div> 
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <div class="menu-item red" style="height:170px;">
+                     <a href="<?php echo base_url().'admin/penjualan'?>" data-toggle="modal">
+                           <i class="fa fa-shopping-bag"></i>
+                            <p style="text-align:left;font-size:14px;padding-left:5px;">Penjualan Eceran</p>
+                      </a>
+                </div> 
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <div class="menu-item blue" style="height:170px;">
+                     <a href="#" data-toggle="modal">
+                           <i class="fa fa-bar-chart"></i>
+                            <p style="text-align:left;font-size:14px;padding-left:5px;">Laporan</p>
+                      </a>
+                </div> 
+            </div>
+            <div class="col-md-3 portfolio-item">
+                <div class="menu-item light-red" style="height:170px;">
+                     <a href="#" data-toggle="modal">
+                           <i class="fa fa-cubes"></i>
+                            <p style="text-align:left;font-size:14px;padding-left:5px;">Pembelian</p>
+                      </a>
+                </div> 
+            </div>
+            <?php }?>
+        </div>
+        
+		
+        <!-- /.row -->
+
+        <footer class="sticky-footer bg-white">
+        
+            <span style="color: white">Copyright &copy; Mulia Baja 2021</span>
+          
+      </footer>
+	
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="<?php echo base_url().'assets/js/jquery.js'?>"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo base_url().'assets/js/bootstrap.min.js'?>"></script>
+
+</body>
+
+</html>
